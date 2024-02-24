f4fc386864254ab9ba134346c8be34382669e2ed
Matheus Douglas Alves Vitor <matheusdouglas58@gmail.com>
2024-02-23 22:32:26 -0300

    Config1

--- RotaseHTTP/02-api-rest-nodejs/src/env/index.ts
+++ RotaseHTTP/02-api-rest-nodejs/src/env/index.ts
@@ -11,11 +11,11 @@ const envSchema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
   DATABASE_CLIENT: z.enum(["sqlite", "pg"]),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
 });
 
 // SafeParse, não dispara um erro, caso a validação falhe
-
+console.log("PORT value from environment:", process.env.PORT);
 const _env = envSchema.safeParse(process.env);
 
 if (_env.success === false) {
